85419fed89bf16dd8b690f5ac212c6f8448573db
Sourave Halder <souravh093@gmail.com>
2024-12-26 21:32:11 +0600

    make the build function onse again

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["firebasestorage.googleapis.com"],
-  }
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "firebasestorage.googleapis.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
